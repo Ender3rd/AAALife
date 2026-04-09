fa8c2e20132873f6ad7e502342ce2d325c21c89d
Owen Smith <Owen.p.Smith@gmail.com>
2026-04-09 10:02:52 -0400

    Some additional thread safety. Ignoring it would be acceptable in this case, but better safe than to start bad habits and create dangerous examples which others might follow not knowing better.

--- src/main/java/com/example/aaalife/interceptors/RateLimiter.java
+++ src/main/java/com/example/aaalife/interceptors/RateLimiter.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+// Massive overkill for what this code should be, but it is a better demonstration of my ability to handle more complicated situations.
 public abstract class RateLimiter implements HandlerInterceptor {
 
     // All the atomics and conncurrents are to make this thread safe while keeping locks for the shortest time possible. In a real application we would probably offload this to an HTTP proxy or a distributed cache.
@@ -25,14 +26,21 @@ public abstract class RateLimiter implements HandlerInterceptor {
 
     @Scheduled(fixedRate = 60 * 1000)
     public void rotateAttemptCounts() {
-        // Gotta love VS code for the handy auto generation. The auto-generated comments suggested it is a monstrosity. I can't say I disagree, but it does the job.
-        userAttempts[3].getAndSet(userAttempts[2].getAndSet(userAttempts[1].getAndSet(userAttempts[0].getAndSet(new ConcurrentHashMap<>()))));
+        // Gotta love VS code for the handy auto generation. The auto-generated comments suggested this method is a monstrosity. I can't say I disagree, but it does the job.
+        synchronized(userAttempts) {
+            userAttempts[3].getAndSet(userAttempts[2].getAndSet(userAttempts[1].getAndSet(userAttempts[0].getAndSet(new ConcurrentHashMap<>()))));
+        }
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String key = getKey(request);
-        int attempt = userAttempts[0].get().compute(key, (k, currentValue) -> { 
+        AtomicReference<ConcurrentHashMap<String, AtomicInteger>>[] attempts;
+        synchronized(userAttempts) {
+            // a shallow copy is exactly what we want. If other threads increase a value, we want to see it. If a rotation happens we want to ignore it.
+            attempts = userAttempts.clone();
+        }
+        int attempt = attempts[0].get().compute(key, (k, currentValue) -> { 
             if (currentValue == null) {
                 return new AtomicInteger(1);
             }
@@ -41,7 +49,7 @@ public abstract class RateLimiter implements HandlerInterceptor {
         }
         ).get();
         // unrolling the loop for performance
-        long additionalAttempts = userAttempts[1].get().getOrDefault(key, DEFAULT_ZERO).get() + userAttempts[2].get().getOrDefault(key, DEFAULT_ZERO).get() + userAttempts[3].get().getOrDefault(key, DEFAULT_ZERO).get() ;
+        long additionalAttempts = attempts[1].get().getOrDefault(key, DEFAULT_ZERO).get() + attempts[2].get().getOrDefault(key, DEFAULT_ZERO).get() + attempts[3].get().getOrDefault(key, DEFAULT_ZERO).get() ;
         if (attempt + additionalAttempts > MAX_RATE) {
             response.setStatus(529); // Being nice to possible legitimate users. In a real application, this might have to be 401 or 403, but that would make testing and legitimate use more difficult.
             // maybe leave it as the result from the app, but have a proxy change the response. OTOH, we would probably just use the proxy to do this anyway.
